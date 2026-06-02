557e90fba99d13715b7cb8483c2dd5049469332d
Pinkikuamri-6299 <pinkikalwar007@gmail.com>
2026-06-02 18:24:23 +0530

    Added Day 2 solutions

---
+++ Week_1/Day_2/Maximum_Subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+     int currmax=0;
+     int max=INT_MIN;
+     for(int i=0;i<nums.size();i++)
+     {
+        currmax+=nums[i];
+        if(currmax >max)
+        {
+            max=currmax;
+        }
+        if(currmax < 0)
+        {
+            currmax=0;
+        }
+     }  
+     return max; 
+    }
+};

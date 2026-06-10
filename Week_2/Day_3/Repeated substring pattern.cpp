b2478b647d8d716efa8fbe1a699d532e1f65c222
Pinkikuamri-6299 <pinkikalwar007@gmail.com>
2026-06-10 20:13:06 +0530

    added day 3 solutions

---
+++ Week_2/Day_3/Repeated substring pattern.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+     int n=s.length();
+     for(int i=1;i<=n/2;i++)
+     {
+        if( n% 1 ==0)
+        {
+            int times = n/i;
+            string pattern =s.substr(0,i);
+            string newstr ="";
+            while(times--)
+            {
+                newstr += pattern;
+            }
+            if(newstr == s)
+            {
+                return true;
+            }
+        }
+     }
+     return false;
+    }
+};
